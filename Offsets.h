72ffc5107370d9fbac46fd2fc2d826cfb7553ae9
Neo7 <164035951+NeoXa7@users.noreply.github.com>
2024-12-17 19:34:27 +0545

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -1,55 +1,76 @@
+#pragma once
+#include <cstddef>
+
 namespace Offsets
 {
-    // buttons;
-    inline int dwForceAttack = 0x181ABC0;
-    inline int dwForceAttack2 = 0x181AC50;
-    inline int dwForceJump = 0x181B0D0;
+	// buttons.hpp/client.dll
+	inline std::ptrdiff_t attack = 0x184E4D0;
+	inline std::ptrdiff_t attack2 = 0x184E560;
+	inline std::ptrdiff_t back = 0x184E7A0;
+	inline std::ptrdiff_t duck = 0x184EA70;
+	inline std::ptrdiff_t forward = 0x184E710;
+	inline std::ptrdiff_t jump = 0x184E9E0;
+	inline std::ptrdiff_t left = 0x184E830;
+	inline std::ptrdiff_t lookatweapon = 0x1A5E1A0;
+	inline std::ptrdiff_t reload = 0x184E440;
+	inline std::ptrdiff_t right = 0x184E8C0;
+	inline std::ptrdiff_t showscores = 0x1A5E080;
+	inline std::ptrdiff_t sprint = 0x184E3B0;
+	inline std::ptrdiff_t turnleft = 0x184E5F0;
+	inline std::ptrdiff_t turnright = 0x184E680;
+	inline std::ptrdiff_t use = 0x184E950;
+	inline std::ptrdiff_t zoom = 0x1A5E110;
 
-    // offsets;
-    inline int dwEntityList = 0x19CA848; // uintptr_t
-    inline int dwLocalPlayerPawn = 0x182FAE8; // uintptr_t
-    inline int dwLocalPlayerController = 0x1A1A690; // uintptr_t
-    inline int dwViewAngles = 0x1A36960; // uintptr_t
-    inline int dwViewMatrix = 0x1A2CAD0; // uintptr_t
-    inline int dwSensitivity = 0x1A29128; // uintptr_t
-    inline int dwSensitivity_sensitivity = 0x40; // uintptr_t
-    inline int dwGameRules = 0x1A28408; // uintptr_t
-    inline int dwGameTypes = 0x1A31B0; // uintptr_t
-    inline int dwGameTypes_mapName = 0x120; // uintptr_t
-    inline int dwPlantedC4 = 0x1A32040; // uintptr_t
-    inline int dwGlobalVars = 0x1823CB0; // uintptr_t
-    inline int dwWeaponC4 = 0x19CDC40;  // uintptr_t
+	// offsets.hpp/client.dll
+	inline std::ptrdiff_t dwCSGOInput = 0x1A5E280;
+	inline std::ptrdiff_t dwEntityList = 0x19F2488;
+	inline std::ptrdiff_t dwGameEntitySystem = 0x1B0CCF8;
+	inline std::ptrdiff_t dwGameEntitySystem_highestEntityIndex = 0x1520;
+	inline std::ptrdiff_t dwGameRules = 0x1A4FE68;
+	inline std::ptrdiff_t dwGlobalVars = 0x18499C0;
+	inline std::ptrdiff_t dwGlowManager = 0x1A4F5A8;
+	inline std::ptrdiff_t dwLocalPlayerController = 0x1A41FD0;
+	inline std::ptrdiff_t dwLocalPlayerPawn = 0x1855CE8;
+	inline std::ptrdiff_t dwPlantedC4 = 0x1A59ED0;
+	inline std::ptrdiff_t dwPrediction = 0x1855B80;
+	inline std::ptrdiff_t dwSensitivity = 0x1A50B88;
+	inline std::ptrdiff_t dwSensitivity_sensitivity = 0x40;
+	inline std::ptrdiff_t dwViewAngles = 0x1A5E650;
+	inline std::ptrdiff_t dwViewMatrix = 0x1A54550;
+	inline std::ptrdiff_t dwViewRender = 0x1A54D60;
+	inline std::ptrdiff_t dwWeaponC4 = 0x19F55F8;
 
+	// offsets.hpp/matchmaking.dll
+	inline std::ptrdiff_t dwGameTypes = 0x1A41B0;
+	inline std::ptrdiff_t dwGameTypes_mapName = 0x120;
 
-    // client_dll;   
-    inline int m_iTeamNum = 0x3E3; // uint8
-    inline int m_iHealth = 0x344; // int32
-    inline int m_ArmorValue = 0x2404; // int32
-    inline int m_hPlayerPawn = 0x80C; // CHandle<C_CSPlayerPawn>
-    inline int m_flFlashBangTime = 0x13F8; // float32
-    inline int m_iIDEntIndex = 0x1458; // CEntityIndex
-    inline int m_iShotsFired = 0x23E4; // int32
-    inline int m_aimPunchAngle = 0x1584; // QAngle
-    inline int m_vOldOrigin = 0x1324; // Vector
-    inline int m_vecViewOffset = 0xCB0; // CNetworkViewOffsetVector
-    inline int m_pGameSceneNode = 0x328; // CGameSceneNode*
-    inline int m_bBombPlanted = 0x9A5; // bool
-    inline int m_bBombDropped = 0x9A4; // bool
-    inline int m_nBombSite = 0xF94; // int32
-    inline int m_bIsScoped = 0x23D0; // bool
-    inline int m_sSanitizedPlayerName = 0x770; // CUtlString
-    inline int m_fFlags = 0x3EC; // uint32
-    inline int m_vecAbsVelocity = 0x3F0; // Vector
-    inline int m_vecAbsOrigin = 0xD0; // Vector
-    inline int m_hOwnerEntity = 0x440; // CHandle<C_BaseEntity>
-    inline int m_iPing = 0x740; // uint32
-    inline int m_bHasExploded = 0xFC5; // bool
-    inline int m_bBeingDefused = 0xFCC; // bool
-    inline int m_flDefuseLength = 0xFDC; // float32
-    inline int m_bWarmupPeriod = 0x41; // bool
-    inline int m_bCTTimeOutActive = 0x4E; // bool
-    inline int m_bTerroristTimeOutActive = 0x4D; // bool
-    inline int m_pCameraServices = 0x11E0; // (CPlayer_CameraServices*)
-    inline int m_iFOV = 0x210; // uint32
-    inline int m_bDormant = 0xEF; // bool
+	// client_dll.hpp/client.dll
+	inline std::ptrdiff_t m_sSanitizedPlayerName = 0x770; // CUtlString
+	inline std::ptrdiff_t m_iTeamNum = 0x3E3; // uint8
+	inline std::ptrdiff_t m_iHealth = 0x344; // int32
+	inline std::ptrdiff_t m_ArmorValue = 0x241C; // int32
+	inline std::ptrdiff_t m_iPing = 0x740; // uint32
+	inline std::ptrdiff_t m_iShotsFired = 0x23FC; // int32
+	inline std::ptrdiff_t m_aimPunchAngle = 0x1584; // QAngle
+	inline std::ptrdiff_t m_vOldOrigin = 0x1324; // Vector
+	inline std::ptrdiff_t m_vecViewOffset = 0xCB0; // CNetworkViewOffsetVector
+	inline std::ptrdiff_t m_vecAbsVelocity = 0x3F0; // Vector
+	inline std::ptrdiff_t m_bIsScoped = 0x23E8; // bool	
+	inline std::ptrdiff_t m_pGameSceneNode = 0x328; // CGameSceneNode*
+	inline std::ptrdiff_t m_bWarmupPeriod = 0x41; // bool
+	inline std::ptrdiff_t m_bBombDropped = 0x9A4; // bool
+	inline std::ptrdiff_t m_bBombPlanted = 0x9A5; // bool
+	inline std::ptrdiff_t m_bBeingDefused = 0xFCC; // bool
+	inline std::ptrdiff_t m_flFlashBangTime = 0x13F8; // float32
+	inline std::ptrdiff_t m_iIDEntIndex = 0x1458; // CEntityIndex
+	inline std::ptrdiff_t m_pCameraServices = 0x11E0; // CPlayer_CameraServices*
+	inline std::ptrdiff_t m_bTerroristTimeOutActive = 0x4D; // bool
+	inline std::ptrdiff_t m_bCTTimeOutActive = 0x4E; // bool
+	inline std::ptrdiff_t m_vecAbsOrigin = 0xD0; // Vector
+	inline std::ptrdiff_t m_nBombSite = 0xF94; // int32
+	inline std::ptrdiff_t m_fFlags = 0x3EC; // uint32
+	inline std::ptrdiff_t m_hOwnerEntity = 0x440; // CHandle<C_BaseEntity>
+	inline std::ptrdiff_t m_hPlayerPawn = 0x80C; // CHandle<C_CSPlayerPawn>
+	inline std::ptrdiff_t m_bHasExploded = 0xFC5; // bool
+	inline std::ptrdiff_t m_flDefuseLength = 0xFDC; // float32
 }
